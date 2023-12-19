081ca7748aec9006d54ff650256d15a20dac31d5
Rajat Singh <59211046+rajat4singh@users.noreply.github.com>
2023-12-19 22:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+var output = "";
+
+for (var i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    output += "FizzBuzz";
+  } else if (i % 3 === 0) {
+    output += "Fizz";
+  } else if (i % 5 === 0) {
+    output += "Buzz";
+  } else {
+    output += i.toString();
+  }
+
+  // Add a newline character after each number/string
+  output += "\n";
+}
+
+// Display the output using alert()
+alert(output);
